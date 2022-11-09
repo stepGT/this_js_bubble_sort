9892abe4561613312f7919e19f9dd1d8726eefc2
Andrey Zuev <stepgt@mail.ru>
2022-11-09 11:18:07 +1000

    Optimized bubbleSort

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const arr = [3, 6, 1, 2, 7, 9, 0, 4, 5, 8];
 
 const bubbleSort = (array) => {
-  for (let i = 0; i < array.length - 1; i++) {
-    for (let j = 0; j < array.length; j++) {
+  const len = array.length;
+  let swapped = false;
+  //
+  for (let i = 0; i < len - 1; i++) {
+    swapped = false;
+    //
+    for (let j = 0; j < len - 1; j++) {
       if (array[j] > array[j + 1]) {
         // swap
         const temp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = temp;
+        swapped = true;
       }
     }
-    console.log(i, array);
+    if (!swapped) break;
   }
   return array;
 };
